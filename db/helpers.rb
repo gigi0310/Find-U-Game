4ce92f8e6be4eccf35d1bf8126744cf0e0da6077
Karen cai <gigi.cai0310@gmail.com>
2021-06-11 07:34:29 +1000

    update db config

--- db/helpers.rb
+++ db/helpers.rb
@@ -1,6 +1,6 @@
 def run_sql(sql, params =[])
     puts ("ENV['DATABASE_URL'] #{ENV['DATABASE_URL']}") 
-    db = PG.connect(ENV['DATABASE'] || {dbname: 'gametracker'})
+    db = PG.connect(ENV['DATABASE_URL'])
     # db = PG.connect(dbname: 'gametracker')
     res = db.exec_params(sql, params)
     db.close
